a68369ce340f9442059da877f27a0c88c6b6d2ac
David McPike <7037366+mcdafydd@users.noreply.github.com>
2018-06-30 15:43:56 -0500

    - openrov/Dockerfile: add default DEBUG flags - openrov/Dockerfile: create all default directories instead of ENTRYPOINT - openrov/Dockerfile: change entrypoint to consolidated start.sh script - mqttclient-mock/start.sh: add socat reuseaddr flag to support multiple senders - docker-compose-dev.yml: add new startup command flags and bind mount to override pino level-change - assets/adjustments.json: logging level that will persist - new startup scripts for enabling debug flags and dev mode in production

--- mqttclient-mock/start.sh
+++ mqttclient-mock/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-socat pty,raw,echo=0,b115200,link=/tmp/link TCP-LISTEN:50000,fork & sleep 1 && USBDEVICE=/tmp/link /usr/local/bin/mqttclient
+socat pty,raw,echo=0,b115200,link=/tmp/link TCP-LISTEN:50000,fork,reuseaddr & sleep 1 && USBDEVICE=/tmp/link /usr/local/bin/mqttclient
---
+++ start-prod-with-debug.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker-compose -f docker-compose.prod-debug.yml up -d
