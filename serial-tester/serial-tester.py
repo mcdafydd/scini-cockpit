dafa0b3fdee868f6175ea8d43312546370e452d2
David McPike <7037366+mcdafydd@users.noreply.github.com>
2018-07-15 00:27:15 -0500

    first commit of serial tester python script and test PRO4 data

---
+++ serial-tester/serial-tester.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import os
+import random
+import select
+import signal
+import struct
+import sys
+import time
+
+serfd = os.open(os.environ['PTY'], os.O_RDWR | os.O_NONBLOCK)
+
+def signal_handler(sig, frame):
+    os.close(serfd)
+    sys.exit(0)
+
+def worker(content):
+    r = []
+    timeout = 1
+
+    while True:
+        try:
+            r, w, e = select.select([serfd],[],[], timeout)
+        except:
+            pass
+
+        if r:
+            data = os.read(serfd,255)
+            print('PRO4 request received = ', data.hex())
+            num = random.randint(0, len(content)-1)
+            msg = content[num]
+            os.write(serfd, bytes.fromhex(msg)
+            print('PRO4 response sent = ', msg)
+        # Test CT sensor
+        #test = struct.pack('13s2B', '01:#059;57600', 0x0d, 0x0a)
+        #test = struct.pack('7s2B', '01:#030', 0x0d, 0x0a)
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    with open('./test.pro4') as f:
+        content = f.readlines()
+    content = [x.strip() for x in content] 
+    worker(content)
+
