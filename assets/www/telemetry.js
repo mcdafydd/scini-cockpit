76a964fed8baf65e9bcf2d14a23193184162eade
David McPike <7037366+mcdafydd@users.noreply.github.com>
2018-10-01 01:52:55 -0500

    - add more telemetry charts - add simple save/restore chart layout feature for telemetry (link in repo issues)

--- assets/www/telemetry.js
+++ assets/www/telemetry.js
@@ -30,20 +30,28 @@ var seriesOptions = [{
 ];
 
 function init() {
-  initChart('cpu', 1, ['cpu']);
-  initChart('camTemp', 5, ['camTemp.8200', 'camTemp.8201', 'camTemp.8202', 'camTemp.8203', 'camTemp.8204']);
-  initChart('depth_p', 4, ['depth_p']);
-  initChart('lights.bus_i', 5, ['lights.bus_i.61', 'lights.bus_i.62', 'lights.bus_i.63', 'lights.bus_i.65', 'lights.bus_i.66']);
-  initChart('lights.bus_v', 5, ['lights.bus_v.61', 'lights.bus_v.62', 'lights.bus_v.63', 'lights.bus_v.65', 'lights.bus_v.66']);
-  initChart('lights.temp', 5, ['lights.temp.61', 'lights.temp.62', 'lights.temp.63', 'lights.temp.65', 'lights.temp.66']);
-  initChart('motors.bus_i', 5, ['motors.bus_i.12', 'motors.bus_i.13', 'motors.bus_i.14', 'motors.bus_i.15', 'motors.bus_i.16']);
-  initChart('motors.bus_v', 5, ['motors.bus_v.12', 'motors.bus_v.13', 'motors.bus_v.14', 'motors.bus_v.15', 'motors.bus_v.16']);
-  initChart('motors.temp', 5, ['motors.temp.12', 'motors.temp.13', 'motors.temp.14', 'motors.temp.15', 'motors.temp.16']);
-  initChart('motors.rpm', 5, ['motors.rpm.12', 'motors.rpm.13', 'motors.rpm.14', 'motors.rpm.15', 'motors.rpm.16']);
-  initMqtt();
+  initChart('cpu', ['cpu']);
+  initChart('camTemp', ['camTemp.8200', 'camTemp.8201', 'camTemp.8202', 'camTemp.8203', 'camTemp.8204']);
+  initChart('depth_p', ['depth_p']);
+  initChart('depth_d', ['depth_d']);
+  initChart('depth_t', ['depth_t']);
+  initChart('imu_p', ['imu_p']);
+  initChart('imu_r', ['imu_r']);
+  initChart('lights.bus_i', ['lights.bus_i.61', 'lights.bus_i.62', 'lights.bus_i.63', 'lights.bus_i.65', 'lights.bus_i.66']);
+  initChart('lights.bus_v', ['lights.bus_v.61', 'lights.bus_v.62', 'lights.bus_v.63', 'lights.bus_v.65', 'lights.bus_v.66']);
+  initChart('lights.temp', ['lights.temp.61', 'lights.temp.62', 'lights.temp.63', 'lights.temp.65', 'lights.temp.66']);
+  initChart('motors.bus_i', ['motors.bus_i.12', 'motors.bus_i.13', 'motors.bus_i.14', 'motors.bus_i.15', 'motors.bus_i.16']);
+  initChart('motors.bus_v', ['motors.bus_v.12', 'motors.bus_v.13', 'motors.bus_v.14', 'motors.bus_v.15', 'motors.bus_v.16']);
+  initChart('motors.temp', ['motors.temp.12', 'motors.temp.13', 'motors.temp.14', 'motors.temp.15', 'motors.temp.16']);
+  initChart('motors.rpm', ['motors.rpm.12', 'motors.rpm.13', 'motors.rpm.14', 'motors.rpm.15', 'motors.rpm.16']);
+  initChart('motors.lift', ['motors.lift']);
+  initChart('motors.pitch', ['motors.pitch']);
+  initChart('motors.strafe', ['motors.strafe']);
+  initChart('motors.throttle', ['motors.throttle']);
+  initChart('motors.yaw', ['motors.yaw']);
 
-  // Create draggable chart grid
-  var grid = new Muuri('.grid', { dragEnabled: true });
+  initMqtt();
+  initGrid();
 }
 
 function initMqtt() {
@@ -77,8 +85,28 @@ function initMqtt() {
   });
 }
 
-function initChart(chartName, numLines, properties) {
+function initGrid() {
+  var grid = new Muuri('.grid', {
+    dragEnabled: true,
+    layoutOnInit: false
+  }).on('move', function () {
+    saveLayout(grid);
+  });
 
+  var layout = window.localStorage.getItem('layout');
+  if (layout) {
+    loadLayout(grid, layout);
+  } else {
+    grid.layout(true);
+  }
+}
+
+function initChart(chartName, properties) {
+
+  let numLines = 1;
+  if (Array.isArray(properties)) {
+    numLines = properties.length;
+  }
   // Build the chart object
   var timeline = new SmoothieChart({
     millisPerPixel: 20,
@@ -97,7 +125,7 @@ function initChart(chartName, numLines, properties) {
   for (var i = 0; i < numLines; i++) {
     let series = new TimeSeries();
     let options = seriesOptions[i];
-    if (properties[i].match('\.[0-9]+$') !== null) {
+    if (properties[i].match(/\.[0-9]+$/) !== null) {
       let labels = properties[i].split(/\./);
       options.tooltipLabel = labels[labels.length-1];
     }
@@ -105,4 +133,37 @@ function initChart(chartName, numLines, properties) {
     timeline.addTimeSeries(series, options);
   }
   timeline.streamTo(document.getElementById(chartName), 1000);
+}
+
+function serializeLayout(grid) {
+  var itemIds = grid.getItems().map(function (item) {
+    return item.getElement().getAttribute('data-id');
+  });
+  return JSON.stringify(itemIds);
+}
+
+function saveLayout(grid) {
+  var layout = serializeLayout(grid);
+  window.localStorage.setItem('layout', layout);
+}
+
+function loadLayout(grid, serializedLayout) {
+  var layout = JSON.parse(serializedLayout);
+  var currentItems = grid.getItems();
+  var currentItemIds = currentItems.map(function (item) {
+    return item.getElement().getAttribute('data-id')
+  });
+  var newItems = [];
+  var itemId;
+  var itemIndex;
+
+  for (var i = 0; i < layout.length; i++) {
+    itemId = layout[i];
+    itemIndex = currentItemIds.indexOf(itemId);
+    if (itemIndex > -1) {
+      newItems.push(currentItems[itemIndex])
+    }
+  }
+
+  grid.sort(newItems, {layout: 'instant'});
 }
