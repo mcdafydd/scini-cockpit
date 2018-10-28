74c7c0adc9a39a989c13c309c58ade9b9f64e7da
David McPike <7037366+mcdafydd@users.noreply.github.com>
2018-10-28 13:54:09 -0500

    - remove software baud rate from imgsrv-mock socat - seems to reduce CPU load for mqttclient

--- imgsrv-mock/start.sh
+++ imgsrv-mock/start.sh
@@ -2,4 +2,4 @@
 
 mjpeg-server.py 8081 &
 http-server.js &
-socat pty,raw,echo=0,b115200,link=/tmp/link TCP-LISTEN:50000,fork,reuseaddr & sleep 1 && USBDEVICE=/tmp/link /usr/local/bin/mqttclient
+socat pty,raw,echo=0,link=/tmp/link TCP-LISTEN:50000,fork,reuseaddr & sleep 1 && USBDEVICE=/tmp/link /usr/local/bin/mqttclient
