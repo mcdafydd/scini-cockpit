416a611ee80b28606c24541bb47bcac4fb5e605b
David McPike <7037366+mcdafydd@users.noreply.github.com>
2018-10-28 18:07:25 -0500

    - add jq to install-prereqs.sh

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -21,6 +21,7 @@ sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
+    jq \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
