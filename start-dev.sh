fc6982ea3859e1f5aea08d83e07fee013e3781f3
David McPike <7037366+mcdafydd@users.noreply.github.com>
2018-11-02 21:01:53 -0500

    - fix startup scripts to handle if openrov container tries to build

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Copy latest web assets if needed in case then container tries to build
+if [ ! -d "openrov/www" ]; then
+  mkdir -p openrov/www
+  rm openrov/www/*
+  cp assets/www/* openrov/www
+  mkdir -p openrov/www-ro
+  rm openrov/www-ro/*
+  cp assets/www-ro/* openrov/www-ro
+fi
+
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
--- start-prod.sh
+++ start-prod.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Copy latest web assets if needed in case then container tries to build
+if [ ! -d "openrov/www" ]; then
+  mkdir -p openrov/www
+  rm openrov/www/*
+  cp assets/www/* openrov/www
+  mkdir -p openrov/www-ro
+  rm openrov/www-ro/*
+  cp assets/www-ro/* openrov/www-ro
+fi
+
 # Get timestamp of current container from compose base file
 CURRENT=`grep scini/openrov docker-compose.yml | awk -F'build-' '{print $2}'`
 CURRENTVER=`date -d $CURRENT +%s`
