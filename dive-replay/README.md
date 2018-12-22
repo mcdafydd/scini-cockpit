27cf9c732bfd845fe96bbd938c0a57e3fc00cc44
David McPike <7037366+mcdafydd@users.noreply.github.com>
2018-12-22 11:41:51 -0600

    separate index for snapshots

--- dive-replay/README.md
+++ dive-replay/README.md
@@ -1,6 +1,7 @@
 # Review video and telemetry from previous dives
 
-1. Script to ingest JSON data into elasticsearch 'telemetry' index - `curl -XPOST 'http://<hostname>:9200/test/test/1' -d @filename.json
-2. Script to create 'images' index linking timestamp from MJPEG image filenames to location in docker volume
-3. Web app to view dives and request 10-second buffers of both images and telemetry and view them on a canvas with basic display controls
-
+1. Script to ingest dive telemetry data and timestamps
+2. Script to ingest dive image file paths and timestamps
+3. Web app to view dives and request buffers of both images and telemetry and view them on a canvas with basic display controls
+4. Kibana charts
+5. Methods to export/update/re-index/share data
--- dive-replay/ingest-images.sh
+++ dive-replay/ingest-images.sh
@@ -4,7 +4,14 @@
 curl -XPUT 'localhost:9200/images' -H 'Content-Type: application/json' -d'
   {
     "mappings": {
-      "_record": {
+      "_video": {
+        "properties": {
+          "time": { "type": "date",
+                    "format": "yyyy_MM_dd_HH_mm_ss_SSS" },
+          "filePath": { "type": "text" }
+        }
+      },
+      "_snapshot": {
         "properties": {
           "time": { "type": "date",
                     "format": "yyyy_MM_dd_HH_mm_ss_SSS" },
@@ -16,10 +23,20 @@ curl -XPUT 'localhost:9200/images' -H 'Content-Type: application/json' -d'
 '
 
 create_record() {
-  fname=`basename $1 | sed 's/\.jpg//'`
+  fname=`basename $1`
+  # send snapshots to a different index
+  if [[ $fname =~ snap_.* ]];
+    index="_snapshot"
+  else
+    index="_video"
+  fi
+
+  # pull timestamp to index from filename
+  ts=`echo $fname | sed 's/^snap_//'`
+  ts=`echo $ts | sed 's/\.jpg//'`
 
   json="time|filePath
-$fname|$1"
+$ts|$1"
 
   record=`jq -Rn '
   ( input  | split("|") ) as $keys |
@@ -27,10 +44,10 @@ $fname|$1"
   [[$keys, $vals] | transpose[] | {key:.[0],value:.[1]}] | from_entries
   ' <<<"$json"`
 
-  curl -XPOST 'http://localhost:9200/images/_record/' -H 'Content-Type: application/json' -d @$record
+  curl -XPOST "http://localhost:9200/images/${index}/" -H 'Content-Type: application/json' -d @$record
 }
 
 export -f create_record
-# Ingest all image paths
+# Ingest all image paths and file create timestamps
 find /opt/openrov/images -name "*.jpg" | xargs -n 1 -P 10 -I {} bash -c 'create_record "{}"'
 
