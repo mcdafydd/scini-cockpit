2fcce473b9281f889f21301b0c52f745a0ddd962
David McPike <7037366+mcdafydd@users.noreply.github.com>
2018-12-22 01:28:23 -0600

    update ingest-images.sh

--- dive-replay/ingest-images.sh
+++ dive-replay/ingest-images.sh
@@ -7,7 +7,7 @@ curl -XPUT 'localhost:9200/images' -H 'Content-Type: application/json' -d'
       "_record": {
         "properties": {
           "time": { "type": "date",
-                    "format": "yyyy_MM_dd_HH_mm_ss_SSS.jpg" },
+                    "format": "yyyy_MM_dd_HH_mm_ss_SSS" },
           "filePath": { "type": "text" }
         }
       }
@@ -15,10 +15,22 @@ curl -XPUT 'localhost:9200/images' -H 'Content-Type: application/json' -d'
   }
 '
 
-# Ingest all telemetry data, auto-index
-# Lots of files, need to break into chunks
-#FILES=`find /opt/openrov/images`
-#for FILE in $FILES; do
-#  curl -XPOST 'http://localhost:9200/images/_record/' -H 'Content-Type: application/json' -d @${FILE}
-#done
+create_record() {
+  fname=`basename $1 | sed 's/\.jpg//'`
+
+  json="time|filePath
+$fname|$1"
+
+  record=`jq -Rn '
+  ( input  | split("|") ) as $keys |
+  ( inputs | split("|") ) as $vals |
+  [[$keys, $vals] | transpose[] | {key:.[0],value:.[1]}] | from_entries
+  ' <<<"$json"`
+
+  curl -XPOST 'http://localhost:9200/images/_record/' -H 'Content-Type: application/json' -d @$record
+}
+
+export -f create_record
+# Ingest all image paths
+find /opt/openrov/images -name "*.jpg" | xargs -n 1 -P 10 -I {} bash -c 'create_record "{}"'
 
