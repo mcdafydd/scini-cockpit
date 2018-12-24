fe85c3ede94ad384ac967c6b4d8a219aec0b6bee
David McPike <7037366+mcdafydd@users.noreply.github.com>
2018-12-24 11:02:39 -0600

    update ingestion scripts

--- dive-replay/ingest-images.sh
+++ dive-replay/ingest-images.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
 
-# Create mapping
-curl -XPUT 'localhost:9200/images' -H 'Content-Type: application/json' -d'
+# Create the two indices
+curl -XPUT "http://localhost:9200/video"
+curl -XPUT "http://localhost:9200/snapshot"
+
+# Create mappings
+curl -XPUT 'http://localhost:9200/video,snapshot/_mapping/_doc' -H 'Content-Type: application/json' -d'
   {
-    "mappings": {
-      "_video": {
-        "properties": {
-          "time": { "type": "date",
-                    "format": "yyyy_MM_dd_HH_mm_ss_SSS" },
-          "filePath": { "type": "text" }
-        }
-      },
-      "_snapshot": {
-        "properties": {
-          "time": { "type": "date",
-                    "format": "yyyy_MM_dd_HH_mm_ss_SSS" },
-          "filePath": { "type": "text" }
-        }
-      }
+    "properties": {
+      "time": { "type": "date",
+                "format": "yyyy_MM_dd_HH_mm_ss_SSS" },
+      "filePath": { "type": "text" }
     }
   }
 '
 
-create_record() {
+generate_body() {
   fname=`basename $1`
-  # send snapshots to a different index
-  if [[ $fname =~ snap_.* ]];
-    index="_snapshot"
-  else
-    index="_video"
-  fi
-
   # pull timestamp to index from filename
   ts=`echo $fname | sed 's/^snap_//'`
   ts=`echo $ts | sed 's/\.jpg//'`
@@ -44,10 +30,44 @@ $ts|$1"
   [[$keys, $vals] | transpose[] | {key:.[0],value:.[1]}] | from_entries
   ' <<<"$json"`
 
-  curl -XPOST "http://localhost:9200/images/${index}/" -H 'Content-Type: application/json' -d @$record
+  echo $record
 }
 
+create_record() {
+  fname=`basename $1`
+  # send snapshots to a different index
+  if [[ $fname =~ snap_.* ]]; then
+    index="snapshot"
+  else
+    index="video"
+  fi
+
+  curl -XPOST "http://localhost:9200/$index/_doc" -H 'Content-Type: application/json' -d"$(generate_body $1)"
+}
+
+export -f generate_body
 export -f create_record
+
+# If low on storage, may need to allow read-write before indexing
+curl -XPUT 'localhost:9200/_settings' -H 'Content-Type: application/json' -d'
+  {
+    "index": {
+      "blocks": {
+        "read_only_allow_delete": "false"
+      }
+    }
+  }
+'
+curl -XPUT 'localhost:9200/video/_settings' -H 'Content-Type: application/json' -d'
+  {
+    "index": {
+      "blocks": {
+        "read_only_allow_delete": "false"
+      }
+    }
+  }
+'
+
 # Ingest all image paths and file create timestamps
 find /opt/openrov/images -name "*.jpg" | xargs -n 1 -P 10 -I {} bash -c 'create_record "{}"'
 
